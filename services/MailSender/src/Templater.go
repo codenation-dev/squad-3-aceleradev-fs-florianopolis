352311a85b2cbae06e10327c42167fc5f0984e03
reb-felipe <psychelipe@gmail.com>
2019-07-17 14:02:15 -0300

    Refactored & add config.go

--- services/MailSender/src/Templater.go
+++ services/MailSender/src/Templater.go
@@ -27,12 +27,11 @@ func TemplateIt(mail Mailrequest,index int) []byte{
 	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n "
 	Header := fmt.Sprintf("To:%s\r\nSubject:%s\r\n",mail.Targets[index].Mail,mail.Subject)
 	var tpl bytes.Buffer
-	tmpl,err := template.ParseFiles("mail/templateone.html")
+	tmpl,err := template.ParseFiles("templates/templateone.html")
 	if(err!=nil){
-		logs.Errorf("Mail - Templater - ",err.Error())
+		logs.Errorf("MailSender_Templater_TemplateIt ",err.Error())
 		panic(err)
 	}
 	tmpl.Execute(&tpl,toTemplate(mail,index))
-	logs.Info("Template: ",tpl.String())
 	return []byte(Header+mime+tpl.String())
 }
