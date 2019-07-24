e7b373001faa97c4ffea036b22afabb80d1fa695
pp5ere <pp5ere@gmail.com>
2019-07-24 11:50:04 -0300

    create historico

--- interfaces/crud/historico/historico.go
+++ interfaces/crud/historico/historico.go
@@ -4,18 +4,21 @@ import (
 	"time"
 	entity "squad-3-aceleradev-fs-florianopolis/entities"
 	db "squad-3-aceleradev-fs-florianopolis/interfaces/db"
-	"strconv"
 	"encoding/json"
 )
 
-func Insert(hist *[]entity.Historico) error {
-	dbi, erro := db.Init()
+//Insert historico
+func Insert(hist *[]entity.FuncPublico) error {
+	dbi, err := db.Init();if err != nil{
+		return err
+	}
 	defer dbi.Database.Close()
 	t := time.Now()
-	formatTime := t.Format(time.RFC1123)
-	json, err := json.Marshal(hist)
-	squery := "INSERT INTO HISTORICO (data, json) VALUES('" + formatTime +
-	 ");"
-	erro = dbi.ExecQuery(squery)
-	return erro
+	formatTime := t.Format("2006-01-02 15:04:05")
+	//formatTime := t.Format(time.RFC3339)
+	json, err := json.Marshal(&hist);if err != nil{
+		return err
+	}
+	 _, err = dbi.Database.Query("INSERT INTO HISTORICO (data, json) VALUES(?, ?)", formatTime, json)
+	return err
 }
