20cfeb20487c8ec192621564caf1659127cff6e6
pp5ere <pp5ere@gmail.com>
2019-07-24 16:08:34 -0300

    add mailGeneral router

--- interfaces/API/router.go
+++ interfaces/API/router.go
@@ -8,7 +8,7 @@ func (a *App) endpoints() {
 	a.Router.HandleFunc("/auth",notImplemented)
 	a.Router.HandleFunc("/warn",notImplemented)
 	a.Router.HandleFunc("/warn/{id}",notImplemented)
-	a.Router.HandleFunc("/mails",notImplemented)
+	a.Router.HandleFunc("/mails", a.mailGeneral)
 	a.Router.HandleFunc("/mails/add",notImplemented)
 	a.Router.HandleFunc("/mails/{id}/delete",notImplemented)
 	a.Router.HandleFunc("/mails/{id}/update",notImplemented)
