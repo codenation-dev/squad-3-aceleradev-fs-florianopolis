df0474cc63a91075d2d8250b35050d2ba9ac4228
pp5ere <pp5ere@gmail.com>
2019-07-24 14:48:48 -0300

    change main

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,27 +1,13 @@
 package main
 
-import ("net/http"
-"squad-3-aceleradev-fs-florianopolis/interfaces/db"
-"squad-3-aceleradev-fs-florianopolis/interfaces/Router"
-"squad-3-aceleradev-fs-florianopolis/entities/logs"
-"fmt")
+import (
+	"fmt"
+	"net/http"
+	"squad-3-aceleradev-fs-florianopolis/entities/logs"
+	api "squad-3-aceleradev-fs-florianopolis/interfaces/API"
+)
 
 func main() {
-	F := database.FakeDB{User:"Leo",Pass:"Leo"}
-	R := router.StartR(F)
-	logs.Errorf("Router ",fmt.Sprintf("Server dead %s",http.ListenAndServe(":8921", R.Router).Error()))
+	R := api.Initialize()
+	logs.Errorf("Router ", fmt.Sprintf("Server dead %s", http.ListenAndServe(":8921", R.Router).Error()))
 }
-
-/*
-import ("squad-3-aceleradev-fs-florianopolis/interfaces/mail")
-
-func main() {
-	TgtList := mailApi.TargetList{}
-	TgtList.Add("Felipe","psychelipe@gmail.com")
-	var FakeINFO []string
-	FakeINFO = append(FakeINFO,"bla")
-	FakeINFO = append(FakeINFO,"bla2") 
-	OtherInfo := "MsgInfoAndOthers"
-	mailApi.Send(OtherInfo,TgtList,OtherInfo,FakeINFO,FakeINFO,OtherInfo)
-}
-*/
