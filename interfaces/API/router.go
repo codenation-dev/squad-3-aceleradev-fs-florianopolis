7f515787f462dbd5e1e5051cc983b5825f75ece7
pp5ere <pp5ere@gmail.com>
2019-07-24 22:07:00 -0300

    add mailRegister

--- interfaces/API/router.go
+++ interfaces/API/router.go
@@ -9,7 +9,7 @@ func (a *App) endpoints() {
 	a.Router.HandleFunc("/warn",notImplemented)
 	a.Router.HandleFunc("/warn/{id}",notImplemented)
 	a.Router.HandleFunc("/mails", a.mailGeneral)
-	a.Router.HandleFunc("/mails/add",notImplemented)
+	a.Router.HandleFunc("/mails/add",a.mailRegister)
 	a.Router.HandleFunc("/mails/{id}/delete",notImplemented)
 	a.Router.HandleFunc("/mails/{id}/update",a.mailEdit)
 	a.Router.HandleFunc("/upload", a.uploadCSV)
