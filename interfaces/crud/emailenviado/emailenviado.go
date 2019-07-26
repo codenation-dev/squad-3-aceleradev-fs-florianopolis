160032a2a42994248fff004a655a356a811d19ff
pp5ere <pp5ere@gmail.com>
2019-07-26 00:22:06 -0300

    implement getall

--- interfaces/crud/emailenviado/emailenviado.go
+++ interfaces/crud/emailenviado/emailenviado.go
@@ -1,6 +1,7 @@
 package emailenviado
 
 import (
+	"strconv"
 	entity "squad-3-aceleradev-fs-florianopolis/entities"
 	"squad-3-aceleradev-fs-florianopolis/entities/logs"
 	db "squad-3-aceleradev-fs-florianopolis/interfaces/db"
@@ -22,3 +23,23 @@ func Insert(emailenviado *entity.EmailEnviado) error {
 	return err
 }
 
+func GetAll(pData time.Time) (*entity.EmailEnviado, error) {
+	var Data time.Time
+	var note entity.Notificacao
+	dbi, erro := db.Init()
+	if erro != nil {
+		logs.Errorf("get(EMAILENVIADO)", erro.Error())
+	}
+	defer dbi.Database.Close()
+	if pData != Data{
+		formatTime := pData.Format("2006-01-02 15:04:05")	
+		_, err := dbi.Database.Query(`select * from EMAILENVIADO order by data desc limit 1`)
+	}else {		
+		seleciona, err := dbi.Database.Query(`select * from NOTIFICACAO order by data desc limit 1`)
+		seleciona.Scan(&note.ID, &note.Data, &note.Lista)
+		seleciona, err = dbi.Database.Query(`select * from EMAILENVIADO where idnotificacao = ` + strconv.Itoa(note.ID))
+		
+	}
+	
+	return err
+}
