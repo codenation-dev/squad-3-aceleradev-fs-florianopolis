bfc685a6b2ffc7ed8e7ba33d65d84e9d97662fef
pp5ere <pp5ere@gmail.com>
2019-07-27 00:58:24 -0300

    changed

--- interfaces/crud/notificacao/notificacao.go
+++ interfaces/crud/notificacao/notificacao.go
@@ -6,33 +6,37 @@ import (
 	"squad-3-aceleradev-fs-florianopolis/entities/logs"
 	db "squad-3-aceleradev-fs-florianopolis/interfaces/db"
 	mail "squad-3-aceleradev-fs-florianopolis/services/MailSender/src"
+
 	utils "squad-3-aceleradev-fs-florianopolis/utils"
+  
 	"strconv"
 	"time"
 )
 
-//GetNextID get the next notificacao id
+
+//GetNextID get the next Notificacao id
 func GetNextID() int {
 	dbi, err := db.Init()
 	defer dbi.Database.Close()
-	if (err!=nil) {
-		logs.Errorf("GetLastNotificacaoID - DB Connection",err.Error())
+	if err != nil {
+		logs.Errorf("GetLastNotificacaoID - DB Connection", err.Error())
 		return 0
 	}
 	type Rsp struct {
-		ID int	
+		ID int
 	}
 	var Response Rsp
-	squery := "SELECT id FROM NOTIFICACAO order by id desc limit 1;" 
+	squery := "SELECT id FROM NOTIFICACAO order by id desc limit 1;"
 	seleciona, err := dbi.Database.Query(squery)
+	defer seleciona.Close()
 	for seleciona.Next() {
-	if (err!=nil) {
-		logs.Errorf("GetLastNotificacaoID",err.Error())
-		return 0
+		if err != nil {
+			logs.Errorf("GetLastNotificacaoID", err.Error())
+			return 0
+		}
+		seleciona.Scan(&Response.ID)
 	}
-	seleciona.Scan(&Response.ID)
-	}
-	return (Response.ID+1)
+	return (Response.ID + 1)
 }
 
 //InsertNotificacao insere uma notificaçao
@@ -46,23 +50,29 @@ func InsertNotificacao(request mail.Mailrequest) error {
 	if erro != nil {
 		logs.Errorf("InsertNotificacao", erro.Error())
 	}
-	_, erro = dbi.Database.Query(`INSERT INTO NOTIFICACAO (data, lista) VALUES(?, ?)`, time.Now().Format("2006-01-02 15:04:05"), response)
+	result, erro := dbi.Database.Query(`INSERT INTO NOTIFICACAO (data, lista) VALUES(?, ?)`, time.Now().Format("2006-01-02 15:04:05"), response)
+	defer result.Close()
+
 	if erro != nil {
 		logs.Errorf("InsertNotificacao", erro.Error())
 	}
 	return erro
 }
 
+
 //Delete Notificacao by ID
 func Delete(id int) error {
 	dbi, erro := db.Init()
 	defer dbi.Database.Close()
 	squery := `DELETE FROM NOTIFICACAO WHERE id = ` + strconv.Itoa(id)
-	_, erro = dbi.Database.Query(squery)
+	result, erro := dbi.Database.Query(squery)
+	defer result.Close()
+
 	return erro
 }
 
 //Get notificacao by ID
+
 func Get(pData time.Time) (*entity.Notificacao, error) {
 	var Data time.Time
 	var note entity.Notificacao
@@ -73,18 +83,19 @@ func Get(pData time.Time) (*entity.Notificacao, error) {
 	defer dbi.Database.Close()
 	if pData != Data {
 		formatTime := pData.Format("2006-01-02 15:04:05")
-		squery := `select * from NOTIFICACAO where data = "`+formatTime+`" limit 1;`	
+		squery := `select * from NOTIFICACAO where data = "` + formatTime + `" limit 1;`
 		seleciona, err := dbi.Database.Query(squery)
+		defer seleciona.Close()
 		if err != nil {
 			return nil, err
 		}
 		for seleciona.Next() {
-		seleciona.Scan(&note.ID, &note.Data, &note.Lista)
+			seleciona.Scan(&note.ID, &note.Data, &note.Lista)
 		}
 
-		}else {		
+	} else {
 		seleciona, err := dbi.Database.Query(`select * from NOTIFICACAO order by data desc limit 1;`)
-		
+		defer seleciona.Close()
 		if err != nil {
 			return nil, err
 		}
@@ -95,6 +106,7 @@ func Get(pData time.Time) (*entity.Notificacao, error) {
 			note.Data = utils.ConvertDateTimeSQL(d)
 		}
 	}
-	
+
 	return &note, nil
+
 }
